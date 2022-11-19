c005e692e1be71ab9744d014a9dadf2f8a0270fd
north74null <grigoryan@null.net>
2022-11-19 10:53:37 +0400

    sixth

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-
-var express = require("express");
+var express = require('express');
 var app = express();
-import main from "./script"
+var server = require('http').createServer(app);
 
-app.get("/", function (req, res) {
-    app.use(express.static("newRepository"))
-    res.send(main)
-    
+
+
+
+app.get('/', function (req, res) {
+    app.use(express.static("."));
+    res.redirect('index.html');
+
 });
 
+server.listen(3000);
 
-app.listen(4000);
+// var express = require("express");
+
+// var app = express();
+
+
+// app.get("/", function (req, res) {
+//     app.use(express.static("newRepository"))
+//     res.send("Hello")
+    
+// });
+
+
+// app.listen(4000);
